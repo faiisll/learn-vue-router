a8a4d9066be332cefd26eb56a84a6582f456b0b8
faiisll <faisal.ayas72@gmail.com>
2022-03-27 19:19:53 +0700

    first commit

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="job">
+    <h1>Jobs Detail Page</h1>
+    <p>{{ job.id }} - {{ job.title }}</p>
+  </div>
+  <div v-else>Loading...</div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err));
+  },
+  data() {
+    return {
+      job: null,
+      // id: this.$route.params.id,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/jobs/JobsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job">
+    <router-link :to="{name: 'jobDetails', params: {id: job.id}}">
+      <h2>{{ job.title }}</h2>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err));
+  },
+};
+</script>
+
+<style></style>
